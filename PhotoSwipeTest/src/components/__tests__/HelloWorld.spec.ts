44a2fe3d9d6bf3cbaa7d955dd048796bb50ae464
Zoeyuan1222 <162100535+Zoeyuan1222@users.noreply.github.com>
2025-05-30 16:04:38 +0800

    补充

--- PhotoSwipeTest/src/components/__tests__/HelloWorld.spec.ts
+++ PhotoSwipeTest/src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import Title from '../Title.vue'
 
-describe('HelloWorld', () => {
+describe('Title', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(Title, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
